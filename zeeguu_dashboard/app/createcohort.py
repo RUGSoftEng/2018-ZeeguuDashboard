2986bb84e6d17f8c5d3622d23566513676cdc29e
christian <christian@debian>
2018-03-17 17:37:27 +0100

    Create classroom flask initialisation
    For some reason using a data validator for the invite code always evaluates to false. Weird because it only needs a string. The next step is to insert the API server address for a POST request.

---
+++ zeeguu_dashboard/app/createcohort.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class CreateCohort(FlaskForm):
+    cohort_name = StringField('Class room name', validators=[DataRequired()])
+    invite_code = StringField('Invite code')
+    language = StringField('Language', validators=[DataRequired()])
+    submit = SubmitField('Create classroom')
