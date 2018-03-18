aeef72de814e17862951372c4b048aef99f39b34
Henry Salas <henryhs97@gmail.com>
2018-03-18 12:11:58 +0100

    Slight design changes

--- zeeguu_dashboard/app/routes.py
+++ zeeguu_dashboard/app/routes.py
@@ -4,24 +4,4 @@ from app import app
 
 @app.route('/')
 def template():
-	classes = [
-        {
-            'class': 'chinese',
-            'id': '1'
-        },
-        {
-            'class': 'dutch',
-            'id': '2'
-        },
-        {
-            'class': 'german',
-            'id': '3'
-         }
-	]
-	return render_template('homepage.html', title='Zeeguu2.0', classes=classes)
-
-@app.route('/class/<idx>')
-def spanish(idx):
-    students = ['John','Mary']
-    idx = int(idx)
-    return render_template('classpage.html', title = 'Spanish class', relevant=students[idx], range=range(10))
+	return render_template('navbar.html', title='Zeeguu2.0')
