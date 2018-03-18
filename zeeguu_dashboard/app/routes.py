6729001b80534533b46c4f2e3ac14d07cff4cde8
Oli26 <lolol1990@hotmail.co.uk>
2018-03-18 16:40:59 -0400

    Fixed routes.py

--- zeeguu_dashboard/app/routes.py
+++ zeeguu_dashboard/app/routes.py
@@ -70,5 +70,13 @@ def spanish(idx):
 def create_classroom():
     form = CreateCohort()
     if form.validate_on_submit():
+        class_name = form.class_name.data
+        inv_code = form.inv_code.data
+        max_students = form.max_students.data
+        teacher_id = form.teacher_id.data
+        class_language_id = form.class_language_id.data
+        package = {'class_name': class_name, 'inv_code': inv_code, 'max_students': max_students,
+                   'teacher_id': teacher_id, 'class_language_id': class_language_id}
+        response = requests.post("http://0.0.0.0:9001/add_class", data=package)
         return redirect('/')
     return render_template('createcohort.html', title = 'Create classroom', form=form)
