425575769a6a953c3c8051ceb510cdeeb12375b4
Oli26 <lolol1990@hotmail.co.uk>
2018-03-18 18:51:47 -0400

    Added loading of classes and users to routes.py. Also added comments for understanding

--- zeeguu_dashboard/app/routes.py
+++ zeeguu_dashboard/app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from app.createcohort import CreateCohort
 import requests
+import json
 path = "http://51.15.89.64:9001/"
 
 
@@ -39,36 +40,66 @@ def template():
             'exercise': 'haahahha'
         }
     ]
+
+
+    #### Example of pulling user data from API #######################################
+    returned_student_ids_string = requests.get(path+"get_users_from_class/1").text
+    print(returned_student_ids_string)
+    returned_student_ids = json.loads(returned_student_ids_string)
+    print(returned_student_ids)
+    for id in returned_student_ids:
+        student_name = requests.get(path+'get_user_name/'+str(id)).text
+        print("Adding student " + student_name)
+        new_student = {
+            'student': student_name,
+            'reading':20,
+            'exercises':30,
+            'article':'place holder'
+        }
+        students.append(new_student)
+    ###############################################################################
+
+
     return render_template('classpage.html', title=classes[0].get('class'), classes=classes, students=students)
 
+#I updated this function to show some functionality to loading data from api.
+#Try add a new class, it works! (if class_id exists. And if teacher_id exists)
+@app.route('/class/<teacher_id>/<class_id>')
+def load_class(teacher_id, class_id):
+    #This loads in the JavaScript object notation of the class id's
+    returned_class_ids_string = requests.get(path + "get_classes_by_teacher_id/"+str(teacher_id)).text
+    #This convers the notation to an int list
+    returned_class_ids = json.loads(returned_class_ids_string)
+    classes = []
+    #For each int in the int list, this will return class_name and add it to classes.
+    for class_id in returned_class_ids:
+        #This loads in json for info(Dictionary) of class with id class_Id
+        class_info_string = requests.get(path+'get_class_info/' + str(class_id)).text
+        #This convers json to dictionary
+        class_info = json.loads(class_info_string)
+        new_class = {
+            #This gets 'class_name' from class_info dictionary
+            'class':class_info['class_name']
+        }
+        classes.append(new_class)
 
-@app.route('/class/<idx>')
-def spanish(idx):
-    students = [
-        {
-            'student': {'username': 'Evi'},
+    returned_student_ids_string = requests.get(path + "get_users_from_class/"+str(class_id)).text
+    returned_student_ids = json.loads(returned_student_ids_string)
+    students = []
+    for id in returned_student_ids:
+        student_name = requests.get(path + 'get_user_name/' + str(id)).text
+        print("Adding student " + student_name)
+        new_student = {
+            'student': student_name,
             'reading': 20,
             'exercises': 30,
-            'last today': {'article': 'skheukf'}
-        },
-
-        {
-            'student': {'username': 'Jakob'},
-            'reading': 30,
-            'exercises': 50,
-            'last today': {'exercise': 'fwgeuiftuwek'}
-        },
-        {
-            'student': {'username': 'Ai'},
-            'reading': 1,
-            'exercises': 2,
-            'last today': {'exercise': 'haahahha'}
+            'article': 'place holder'
         }
-    ]
-    idx = int(idx)
-    return render_template('classpage.html', title = 'Spanish class', relevant=students[idx], range=range(3))
+        students.append(new_student)
+    return render_template('classpage.html', title = 'Spanish class', classes=classes, students=students, range=range(3))
 
 
+# This works if class_inv is not taken and teacher_id exists.
 @app.route('/create_classroom',  methods=['GET', 'POST'])
 def create_classroom():
     form = CreateCohort()
