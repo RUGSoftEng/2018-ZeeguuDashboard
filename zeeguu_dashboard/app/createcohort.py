a0cbf3cca421f64f189ac2bffe5b3f239323bb9f
Oli26 <lolol1990@hotmail.co.uk>
2018-03-18 16:42:27 -0400

    fixing createcohort.py after failed merge

--- zeeguu_dashboard/app/createcohort.py
+++ zeeguu_dashboard/app/createcohort.py
@@ -6,4 +6,5 @@ class CreateCohort(FlaskForm):
     cohort_name = StringField('Class room name', validators=[DataRequired()])
     invite_code = StringField('Invite code')
     language = StringField('Language', validators=[DataRequired()])
+    teacher_id = StringField('Teacher id', validators=[DataRequired()])
     submit = SubmitField('Create classroom')
