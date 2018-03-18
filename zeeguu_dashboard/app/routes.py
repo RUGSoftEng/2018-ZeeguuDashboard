5c2a720140101e7ca8c96139bb3a1d76db0171e8
Henry Salas <henryhs97@gmail.com>
2018-03-18 13:58:00 +0100

    create class page added (no functionality yet)

--- zeeguu_dashboard/app/routes.py
+++ zeeguu_dashboard/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.createcohort import CreateCohort
+import requests
 
 @app.route('/')
 def template():
@@ -30,6 +31,15 @@ def spanish(idx):
 def create_classroom():
     form = CreateCohort()
     if form.validate_on_submit():
-        return redirect('/')
+        #return requests.get("http://0.0.0.0:9001/get_user_name/1").text
+        class_name = form.class_name.data
+        inv_code = form.inv_code.data
+        max_students = form.max_students.data
+        teacher_id = form.teacher_id.data
+        class_language_id = form.class_language_id.data
+        package = {'class_name':class_name, 'inv_code':inv_code, 'max_students':max_students, 'teacher_id':teacher_id, 'class_language_id':class_language_id}
+        response = requests.post("http://0.0.0.0:9001/add_class", data = package)
+        print(response)
+
     return render_template('createcohort.html', title = 'Create classroom', form=form)
 
