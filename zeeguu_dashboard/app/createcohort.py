186fbff74ef5217e8501acad4a2191f4c32f5d25
Henry Salas <henryhs97@gmail.com>
2018-03-18 17:17:26 +0100

    Initial beta of dashboard.

--- zeeguu_dashboard/app/createcohort.py
+++ zeeguu_dashboard/app/createcohort.py
@@ -3,9 +3,7 @@ from wtforms import StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class CreateCohort(FlaskForm):
-    class_name = StringField('Class room name', validators=[DataRequired()])
-    inv_code = StringField('Invite code')
-    class_language_id = StringField('Language', validators=[DataRequired()])
-    max_students = StringField('Max size', validators=[DataRequired()])
-    teacher_id = StringField('Teacher id', validators=[DataRequired()])
+    cohort_name = StringField('Class room name', validators=[DataRequired()])
+    invite_code = StringField('Invite code')
+    language = StringField('Language', validators=[DataRequired()])
     submit = SubmitField('Create classroom')
--- zeeguu_dashboard/app/routes.py
+++ zeeguu_dashboard/app/routes.py
@@ -1,45 +1,74 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.createcohort import CreateCohort
-import requests
 
 @app.route('/')
 def template():
     classes = [
         {
-            'class': 'chinese',
-            'id': '1'
+            'class': 'Spanish A1',
         },
         {
-            'class': 'dutch',
-            'id': '2'
+            'class': 'Spanish B2',
         },
         {
-            'class': 'german',
-            'id': '3'
+            'class': 'Spanish C1',
         }
     ]
-    return render_template('homepage.html', title='Zeeguu2.0', classes=classes)
+    students = [
+        {
+            'student': 'Evi',
+            'reading': 20,
+            'exercises': 30,
+            'article': 'skheukf'
+        },
+
+        {
+            'student': 'Jakob',
+            'reading': 30,
+            'exercises': 5,
+            'exercise': 'fwgeuiftuwek'
+        },
+        {
+            'student': 'Ai',
+            'reading': 1,
+            'exercises': 55,
+            'exercise': 'haahahha'
+        }
+    ]
+    return render_template('classpage.html', title=classes[0].get('class'), classes=classes, students=students)
+
 
 @app.route('/class/<idx>')
 def spanish(idx):
-    students = ['John','Mary']
+    students = [
+        {
+            'student': {'username': 'Evi'},
+            'reading': 20,
+            'exercises': 30,
+            'last today': {'article': 'skheukf'}
+        },
+
+        {
+            'student': {'username': 'Jakob'},
+            'reading': 30,
+            'exercises': 50,
+            'last today': {'exercise': 'fwgeuiftuwek'}
+        },
+        {
+            'student': {'username': 'Ai'},
+            'reading': 1,
+            'exercises': 2,
+            'last today': {'exercise': 'haahahha'}
+        }
+    ]
     idx = int(idx)
-    return render_template('classpage.html', title = 'Spanish class', relevant=students[idx], range=range(10))
+    return render_template('classpage.html', title = 'Spanish class', relevant=students[idx], range=range(3))
+
 
 @app.route('/create_classroom',  methods=['GET', 'POST'])
 def create_classroom():
     form = CreateCohort()
     if form.validate_on_submit():
-        #return requests.get("http://0.0.0.0:9001/get_user_name/1").text
-        class_name = form.class_name.data
-        inv_code = form.inv_code.data
-        max_students = form.max_students.data
-        teacher_id = form.teacher_id.data
-        class_language_id = form.class_language_id.data
-        package = {'class_name':class_name, 'inv_code':inv_code, 'max_students':max_students, 'teacher_id':teacher_id, 'class_language_id':class_language_id}
-        response = requests.post("http://0.0.0.0:9001/add_class", data = package)
-        print(response)
-
+        return redirect('/')
     return render_template('createcohort.html', title = 'Create classroom', form=form)
-
