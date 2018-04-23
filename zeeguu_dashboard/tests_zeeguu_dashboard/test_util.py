fdabc51e11035429418dcab0ace51558328f1304
ctfgm <ctfgm@protonmail.ch>
2018-04-23 16:42:47 +0200

    Skeleton for pytest
    Signed-off-by: ctfgm <ctfgm@protonmail.ch>

---
+++ zeeguu_dashboard/tests_zeeguu_dashboard/test_util.py
@@ -0,0 +1,42 @@
+def func(x):
+    return x
+
+
+def test_load_user_info():
+    assert func(0) == 0
+
+
+def test_remove_class():
+    assert func(0) == 0
+
+
+def test_load_class_info():
+    assert func(0) == 0
+
+
+def test_load_classes():
+    assert func(0) == 0
+
+
+def test_load_students():
+    assert func(0) == 0
+
+
+def test_load_user_data():
+    assert func(0) == 0
+
+
+def test_filter_user_bookmarks():
+    assert func(0) == 0
+
+
+def test_verify_invite_code_exists():
+    assert func(0) == 0
+
+
+def test_api_post():
+    assert func(0) == 0
+
+
+def test_api_get():
+    assert func(0) == 0
