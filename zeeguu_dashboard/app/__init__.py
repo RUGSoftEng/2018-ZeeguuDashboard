93b296afcaf58dd1be7f175d52e818f672e9f0ed
JakobVokac <jakob.vokac@gmail.com>
2018-05-06 12:03:06 +0200

    fixed up the config file

--- zeeguu_dashboard/app/__init__.py
+++ zeeguu_dashboard/app/__init__.py
@@ -1,16 +1,5 @@
-import sys
 from flask import Flask
-from config import Config
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config.from_object(Config)
-bootstrap = Bootstrap(app)
 
 from app.page_routes import homepage, errorpages, login, studentpage, classroom
-
-arguments = sys.argv
-if len(arguments) > 0:
-    for arg in arguments:
-        if arg == 'Debug':
-            app.debug = True
