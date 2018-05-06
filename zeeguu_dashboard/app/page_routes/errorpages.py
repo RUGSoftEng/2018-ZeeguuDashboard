143fc5b7b8ff240ae86ad621988739bc9d921602
Oli26 <lolol1990@hotmail.co.uk>
2018-05-06 08:34:41 -0400

    Fixed routing to permissions page

--- zeeguu_dashboard/app/page_routes/errorpages.py
+++ zeeguu_dashboard/app/page_routes/errorpages.py
@@ -5,23 +5,24 @@ from app import app
 """
 This file contains all of the error page routes.
 """
+@app.errorhandler(401)
+@app.route("/401")
+def invalid_credentials():
+    print("called 401")
+    """
+    :param e:
+    :return:
+    """
+    return render_template("errorpage.html", exception="401 UNAUTHORIZED")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    print("called 404")
     """
-
     :param e:
     :return:
     """
-    return render_template("errorpage.html", exception=e)
+    return render_template("errorpage.html", exception="404 Not Found: The requested URL was not found on the server.")
 
 
-@app.errorhandler(401)
-def invalid_credentials(e):
-    """
-
-    :param e:
-    :return:
-    """
-    return render_template("errorpage.html", exception=e)
