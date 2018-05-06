cd5aecaa1a891a02da4cdbb4642c772ac2820837
ctfgm <ctfgm@protonmail.ch>
2018-05-06 17:43:54 +0200

    Tests going; Can't yet mock the text call on api_get in test_load_class_info
    Signed-off-by: ctfgm <ctfgm@protonmail.ch>

--- zeeguu_dashboard/app/test_foo.py
+++ zeeguu_dashboard/app/test_foo.py
@@ -31,6 +31,17 @@ class TestCase(unittest.TestCase):
         classroom.remove_class(class_id)
         mock_api_connection.api_post.assert_called_with('remove_cohort/' + str(class_id), dict)
 
+    @patch('app.util.classroom.api_connection')
+    @patch('app.util.classroom.json')
+    def test_load_class_info(self, mock_json, mock_api_connection):
+        # TODO(christian): test the api_get().text return
+        class_id = 0
+        mock_json.loads.return_value = 'json return value'
+
+        class_info = classroom.load_class_info(class_id)
+
+        assert class_info == 'json return value'
+
 
 if __name__ == '__main__':
     unittest.main()
