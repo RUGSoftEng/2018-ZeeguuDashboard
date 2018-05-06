6e9be8d4d492bdcbb47f02065f6cc8cd2a266afd
JakobVokac <jakob.vokac@gmail.com>
2018-05-06 14:37:16 +0200

    some comments

--- zeeguu_dashboard/app/page_routes/homepage.py
+++ zeeguu_dashboard/app/page_routes/homepage.py
@@ -10,31 +10,21 @@ This file contains the routes for the homepage.
 """
 
 
-"""
-This route redirects to the homepage, when only the URL is searched by the browser (for convenience).
-"""
-
-
 @app.route('/')
 def to_homepage():
     """
-
-    :return:
+    This route redirects to the homepage, when only the URL is searched by the browser (for convenience).
+    :return: Redirects to '/teacher' endpoint.
     """
     return redirect("/teacher")
-
-
-"""
-This shows a teachers corresponding homepage, as long as a session is validated.
-"""
 
 
 @app.route('/teacher/')
 @has_session
 def homepage():
     """
-
-    :return:
+    This shows a teachers corresponding homepage, as long as a session is validated.
+    :return: 
     """
     classes = load_classes()
     return render_template('homepage.html', title="Homepage", classes=classes)
