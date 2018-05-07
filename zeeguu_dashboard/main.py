c086774e8e991aea65bc959b0201a765cc5fd98f
ctfgm <ctfgm@protonmail.ch>
2018-05-07 19:01:01 +0200

    1 less instance
    Signed-off-by: ctfgm <ctfgm@protonmail.ch>

--- zeeguu_dashboard/main.py
+++ zeeguu_dashboard/main.py
@@ -15,4 +15,4 @@ app.config.from_pyfile(os.environ.get("ZEEGUU_DASHBOARD_CONFIG"), silent=False)
 app.config["API_PATH"] = "http://" + os.environ.get("API_PATH") + "/"
 bootstrap = Bootstrap(app)
 
-app.run()
+app.run(use_reloader=False)
