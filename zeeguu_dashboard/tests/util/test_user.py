abd822b4764777ef5a73b26b782f2ae1017509f4
ctfgm <ctfgm@protonmail.ch>
2018-05-07 18:26:36 +0200

    Testing util/user.py is done
    Signed-off-by: ctfgm <ctfgm@protonmail.ch>

--- zeeguu_dashboard/tests/util/test_user.py
+++ zeeguu_dashboard/tests/util/test_user.py
@@ -24,3 +24,29 @@ class TestUser(unittest.TestCase):
             mock.text = '["foo", {"bar":["baz", null, 1.0, 2]}]'
             mock_api_get.return_value = mock
             assert user.load_user_info(user_id) == json.loads(mock.text)
+
+    @patch('app.util.user.api_get')
+    def test_load_user_data(self, mock_api_get):
+        day0 = {'bookmarks': [{'from': 'meer'}, {'from': 'zout'}, {'from': 'GRANATE'}]}
+        day1 = {'bookmarks': [{'from': 'hallo'}, {'from': 'alsjeblieft'}, {'from': 'alsjeblieft'}]}
+        days = [day0, day1]
+        expected_result = [day0, {'bookmarks': [{'from': 'hallo'}, {'from': 'alsjeblieft'}]}]
+        app = flask.Flask(__name__)
+
+        with app.test_request_context('/?name=Chris'):
+            mock = MagicMock()
+            mock.text = json.dumps(days)
+            mock_api_get.return_value = mock
+
+            user_id = 0
+            time = 0
+            assert user.load_user_data(user_id, time) == expected_result
+
+    def test_filter_user_bookmarks(self):
+        day0 = {'bookmarks': [{'from': 'meer'}, {'from': 'zout'}, {'from': 'GRANATE'}]}
+        day1 = {'bookmarks': [{'from': 'hallo'}, {'from': 'alsjeblieft'}, {'from': 'alsjeblieft'}]}
+        days = [day0, day1]
+        expected_result = [day0, {'bookmarks': [{'from': 'hallo'}, {'from': 'alsjeblieft'}]}]
+
+        real_result = user.filter_user_bookmarks(days)
+        assert real_result == expected_result
