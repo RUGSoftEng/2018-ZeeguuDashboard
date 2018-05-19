ee31e0089451584c53ca7abf1d7fac3704ea7460
Oli26 <lolol1990@hotmail.co.uk>
2018-05-19 09:15:49 -0400

    Fixed default time and error with time being smaller than table_time

--- zeeguu_dashboard/app/forms/edit_cohort.py
+++ zeeguu_dashboard/app/forms/edit_cohort.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from app.util.classroom import verify_invite_code_exists
 
 
 class EditCohort(FlaskForm):
@@ -8,6 +9,23 @@ class EditCohort(FlaskForm):
     This class extends FlaskForm. It is used when editing class information.
     """
     class_name = StringField('Class room name', validators=[DataRequired()])
-    inv_code = StringField('Invite code')
+    inv_code = StringField('Invite code', validators=[DataRequired()])
     max_students = StringField('Max students', validators=[DataRequired()])
     submit = SubmitField('Create classroom')
+
+    def validate(self):
+        """
+        This function validates the EditCohort form.
+        It extends from the normal validation as we need to validate whether some
+        filled in data is already in use (the class invite code).
+        :return: Returns a boolean, indicating whether the form is properly filled out or not.
+        """
+        if not FlaskForm.validate(self):
+            return False
+        if verify_invite_code_exists(self.inv_code.data):
+            print("Code already in use!")
+            tmp = list(self.inv_code.errors)
+            tmp.append("Code already in use!")
+            self.inv_code.errors = tuple(tmp)
+            return False
+        return True
