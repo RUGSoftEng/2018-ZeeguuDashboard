cdcf48e338b5846cb9edda1610a44b78b8333cd5
JakobVokac <jakob.vokac@gmail.com>
2018-06-09 15:23:26 +0200

    fixed backend for breadcrumbs

--- zeeguu_dashboard/app/page_routes/studentpage.py
+++ zeeguu_dashboard/app/page_routes/studentpage.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, request
 
 from app import app
+from app.util.classroom import load_class_info
 from app.util.permissions import has_student_permission
 from app.util.user import load_user_data, load_user_info, filter_user_bookmarks, get_correct_time
 
@@ -9,9 +10,9 @@ This file contains the routes for a student page.
 """
 
 
-@app.route('/student/<student_id>/', methods=['GET'])
+@app.route('/class/<class_id>/student/<student_id>/', methods=['GET'])
 @has_student_permission
-def student_page(student_id):
+def student_page(class_id,student_id):
     """
     This loads the student page. When a cookie is set, it's used to set the time filter to show.
     Otherwise, the default_time is used as requested by the customer.
@@ -24,11 +25,11 @@ def student_page(student_id):
     bookmarks = load_user_data(user_id=student_id, time=time)
     info = load_user_info(student_id, time)
     bookmarks = filter_user_bookmarks(bookmarks)
-
+    class_name = load_class_info(class_id)["name"]
     time = get_correct_time(time)
 
     if not bookmarks or not info:
-        return render_template("empty_student_page.html", info=info, title=info['name'], student_id=student_id,
-                               time=time)
-    return render_template("studentpage.html", title=info['name'], info=info, stats=bookmarks, student_id=student_id,
-                           time=time)
+        return render_template("empty_student_page.html", info=info, title=info['name'], class_id=class_id,
+                               class_name = class_name, student_id=student_id, time=time)
+    return render_template("studentpage.html", title=info['name'], info=info, stats=bookmarks, class_id=class_id,
+                           class_name=class_name, student_id=student_id, time=time)
