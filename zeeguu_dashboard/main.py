fe24756cb2a326a7851f0624726e6b3949890d59
Mircea Lungu <mircea.lungu@gmail.com>
2018-11-08 15:55:21 +0100

    python server

--- zeeguu_dashboard/main.py
+++ zeeguu_dashboard/main.py
@@ -11,8 +11,12 @@ if len(arguments) > 0:
         if arg == 'Debug':
             app.debug = True
 
+print(os.environ.get("ZEEGUU_DASHBOARD_CONFIG"))
+
 app.config.from_pyfile(os.environ.get("ZEEGUU_DASHBOARD_CONFIG"), silent=False)
-app.config["API_PATH"] = os.environ.get("ZEEGUU_API_URL") + "/"
 bootstrap = Bootstrap(app)
 
-app.run(use_reloader=False)
+print("Running Teacher Dashboard with config: ")
+print (app.config)
+
+app.run(use_reloader=True)
